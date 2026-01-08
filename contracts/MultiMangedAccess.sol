5cbb7085b347eac99932ce7c56076b1ce103fe38
becky5432 <becky5432@naver.com>
2026-01-08 19:11:57 +0900

    add multi managed access

---
+++ contracts/MultiMangedAccess.sol
@@ -0,0 +1,55 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+abstract contract MultiManagedAccess {
+    uint256 constant MANAGER_NUMBERS = 5;
+    address public owner;
+    address[MANAGER_NUMBERS] public managers;
+    bool[MANAGER_NUMBERS] public confirmed;
+
+    constructor(address _owner, address[5] memory _managers) {
+        owner = _owner;
+        for (uint i = 0; i < MANAGER_NUMBERS; i++) {
+            managers[i] = _managers[i];
+        }
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not authorized");
+        _;
+    }
+
+    function allConfirmed() internal view returns (bool) {
+        for (uint i = 0; i < MANAGER_NUMBERS; i++) {
+            if (!confirmed[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function reset() internal {
+        for (uint i = 0; i < MANAGER_NUMBERS; i++) {
+            confirmed[i] = false;
+        }
+    }
+
+    modifier onlyManager() {
+        bool found = false;
+        for (uint i = 0; i < MANAGER_NUMBERS; i++) {
+            if (managers[i] == msg.sender) {
+                found = true;
+                confirmed[i] = true;
+                break;
+            }
+        }
+        require(found, "You are not a manager");
+        _;
+    }
+
+    modifier onlyAllConfirmed() {
+        require(allConfirmed(), "Not all confirmed yet");
+        reset();
+        _;
+    }
+}
