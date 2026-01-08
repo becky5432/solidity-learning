9a256b865870cf0a4d57990f8852e636c8a41647
becky5432 <becky5432@naver.com>
2026-01-08 19:30:22 +0900

    attach MultiManagedAccess to TinyBank

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -2,9 +2,8 @@
 pragma solidity ^0.8.28;
 
 import "./MangedAccess.sol";
+import "./MultiMangedAccess.sol";
 
-//deposit / withdraw
-//user --> deposit --> TinyBank --> transfer(user --> TinyBank)
 interface IMyToken {
     function transfer(uint256 amount, address to) external;
 
@@ -13,7 +12,7 @@ interface IMyToken {
     function mint(uint256 amount, address owner) external;
 }
 
-contract TinyBank is ManagedAccess {
+contract TinyBank is MultiManagedAccess {
     event Staked(address from, uint256 amount);
     event Withdraw(uint256 amount, address to);
 
@@ -27,7 +26,10 @@ contract TinyBank is ManagedAccess {
     mapping(address => uint256) public staked;
     uint256 public totalStaked;
 
-    constructor(IMyToken _stakingToken) ManagedAccess(msg.sender, msg.sender) {
+    constructor(
+        IMyToken _stakingToken,
+        address[5] memory _managers
+    ) MultiManagedAccess(msg.sender, _managers) {
         stakingToken = _stakingToken;
         rewardPerBlock = defaultRewardPerBlock;
     }
@@ -43,7 +45,9 @@ contract TinyBank is ManagedAccess {
         _; //caller
     }
 
-    function setRewardPerBlock(uint256 _amount) external onlyManager {
+    function setRewardPerBlock(
+        uint256 _amount
+    ) external onlyManager onlyAllConfirmed {
         rewardPerBlock = _amount;
     }
 
